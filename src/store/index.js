1b955adbcf68ea80be97820cdfbbb787de03a5bb
Batr Kanzitelli <kanzitdinov@gmail.com>
2019-11-14 00:23:44 -0800

    redux store persistance added

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react'
 import thunkMiddleware from 'redux-thunk';
 
 import reducer from './reducers';
 
+const persistConfig = {
+    key: 'rootKeyPersist',
+    storage: AsyncStorage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
 const store = createStore(
-    reducer,
+    persistedReducer,
     applyMiddleware(thunkMiddleware)
 );
+const persistor = persistStore(store);
 
 export const withReduxProvider = C => props => (
     <Provider store={store}>
-        <C {...props}/>
+        <PersistGate loading={null} persistor={persistor}>
+            <C {...props}/>
+        </PersistGate>
     </Provider>
 );
