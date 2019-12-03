08f3717fc83281a8c58d87a1e8b8f589865e2ada
Batr Kanzitelli <kanzitdinov@gmail.com>
2019-12-02 21:40:21 -0800

    tab name --> Redux App

--- srcRedux/App.ts
+++ srcRedux/App.ts
@@ -66,7 +66,7 @@ export const startApp = () => {
                             }],
                             options: {
                                 bottomTab: {
-                                    text: 'Emptiness',
+                                    text: 'Redux App',
                                     icon: reactIcon,
                                 },
                             },
