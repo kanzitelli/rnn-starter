8c4afda6482e8c99e6426d2f0925ca1ada46055e
Batr Kanzitelli <kanzitdinov@gmail.com>
2020-02-19 20:40:52 +0300

    start mobx app

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ import { Navigation } from 'react-native-navigation';
 // please comment unnecessary import, this is important
 // because RNN registers screens for both of them if two imports are presented
 
-import { startApp as startReduxApp } from './srcRedux/App';
-// import { startApp as startMobXApp } from './srcMobX/App';
+// import { startApp as startReduxApp } from './srcRedux/App';
+import { startApp as startMobXApp } from './srcMobX/App';
 
 Navigation.events().registerAppLaunchedListener(() => {
-    startReduxApp();
-    // startMobXApp();
+    // startReduxApp();
+    startMobXApp();
 });
