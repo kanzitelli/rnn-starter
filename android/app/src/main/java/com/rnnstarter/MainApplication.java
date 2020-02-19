f322c159b8cb4e71eca202501a139c8e354c039e
Batr Kanzitelli <kanzitdinov@gmail.com>
2020-02-19 03:12:57 +0300

    new logic for MainApplication.java

--- android/app/src/main/java/com/rnnstarter/MainApplication.java
+++ android/app/src/main/java/com/rnnstarter/MainApplication.java
@@ -1,49 +1,37 @@
 package com.rnnstarter;
 
-import android.app.Application;
-import android.content.Context;
-import java.lang.reflect.InvocationTargetException;
-import java.util.List;
-import java.util.Arrays;
-
 import com.facebook.react.PackageList;
-import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.soloader.SoLoader;
 
 import com.reactnativenavigation.NavigationApplication;
 import com.reactnativenavigation.react.NavigationReactNativeHost;
-import com.reactnativenavigation.react.ReactGateway;
+
+import java.util.List;
 
 public class MainApplication extends NavigationApplication {
-    @Override
-    protected ReactGateway createReactGateway() {
-        ReactNativeHost host = new NavigationReactNativeHost(this, isDebug(), createAdditionalReactPackages()) {
-            @Override
-            protected String getJSMainModuleName() {
-                return "index";
-            }
-        };
-        ReactGateway gateway = new ReactGateway(this, isDebug(), host);
-        return gateway;
-    }
+    private final ReactNativeHost host = new NavigationReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+            return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+            List<ReactPackage> packages = new PackageList(this).getPackages();
+            // Packages that cannot be auto linked yet can be added manually here, for example:
+            // packages.add(new MyReactNativePackage());
+            return packages;
+        }
+
+        @Override
+        protected String getJSMainModuleName() {
+            return "index";
+        }
+    };
 
     @Override
-    public boolean isDebug() {
-        return BuildConfig.DEBUG;
-    }
-
-    protected List<ReactPackage> getPackages() {
-      @SuppressWarnings("UnnecessaryLocalVariable")
-      List<ReactPackage> packages = new PackageList(this).getPackages();
-      // Packages that cannot be autolinked yet can be added manually here, for example:
-      // packages.add(new MyReactNativePackage());
-      return packages;
-    }
-
-    @Override
-    public List<ReactPackage> createAdditionalReactPackages() {
-        return getPackages();
+    public ReactNativeHost getReactNativeHost() {
+        return host;
     }
 }
