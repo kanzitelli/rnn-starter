f8cc07d3c5f3186121f310857f8310bd3fcc2398
Batr Kanzitelli <kanzitdinov@gmail.com>
2020-08-12 13:03:22 +0200

    removing expo-splash-screen from android

--- android/app/src/main/java/com/rnn_starter/generated/BasePackageList.java
+++ android/app/src/main/java/com/rnn_starter/generated/BasePackageList.java
@@ -17,8 +17,8 @@ public class BasePackageList {
         new expo.modules.location.LocationPackage(),
         new expo.modules.network.NetworkPackage(),
         new expo.modules.permissions.PermissionsPackage(),
-        new expo.modules.splashscreen.SplashScreenPackage(),
-        new expo.modules.sqlite.SQLitePackage()
+        new expo.modules.sqlite.SQLitePackage(),
+        new expo.modules.updates.UpdatesPackage()
     );
   }
 }
