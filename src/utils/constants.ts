be3b0f278fb63e5f080040e541897ff69e1576cd
Batr Kanzitelli <kanzitdinov@gmail.com>
2020-10-05 00:09:57 +0200

    global app constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,20 @@
+class Contants {
+  ScreenNames = {
+    CounterScreen: 'rnn_starter.CounterScreen',
+    ExpoScreen: 'rnn_starter.ExpoScreen',
+    AboutScreen: 'rnn_starter.AboutScreen',
+  }
+  BottomTabsTitles = {
+    tab1: 'Counter',
+    tab2: 'Expo',
+    tab3: 'About',
+  }
+  CounterScreen = {
+    decButtonId: 'decButtonId',
+    decButtonTitle: 'Dec',
+    incButtonId: 'incButtonId',
+    incButtonTitle: 'Inc',
+  }
+}
+
+export default new Contants();
