6ed2358ea83b3d7b9ee15ee9daad528a944a4f58
Batr Kanzitelli <kanzitdinov@gmail.com>
2020-10-06 01:42:56 +0200

    excluded deps back

--- android/app/src/main/java/com/rnn_starter/generated/BasePackageList.java
+++ android/app/src/main/java/com/rnn_starter/generated/BasePackageList.java
@@ -9,10 +9,12 @@ public class BasePackageList {
     return Arrays.<Package>asList(
         new expo.modules.constants.ConstantsPackage(),
         new expo.modules.errorrecovery.ErrorRecoveryPackage(),
+        new expo.modules.filesystem.FileSystemPackage(),
         new expo.modules.font.FontLoaderPackage(),
         new expo.modules.imageloader.ImageLoaderPackage(),
         new expo.modules.keepawake.KeepAwakePackage(),
         new expo.modules.lineargradient.LinearGradientPackage(),
+        new expo.modules.location.LocationPackage(),
         new expo.modules.network.NetworkPackage(),
         new expo.modules.permissions.PermissionsPackage(),
         new expo.modules.sqlite.SQLitePackage(),
