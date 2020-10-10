312ca95a067f3bb5a2629a9a42bd1677cd4ea42b
Batr Kanzitelli <kanzitdinov@gmail.com>
2020-10-10 20:52:17 +0200

    registerRootComponent added
    otherwise, it was causing a crash on iOS in release build

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import 'expo-asset';
+import 'react-native-gesture-handler';
 import { Navigation } from 'react-native-navigation';
+import { registerRootComponent } from 'expo';
 
 import { startApp } from './src/App';
+import ExpoApp from './src/screens/ExpoAppScreen';
 
+registerRootComponent(ExpoApp); // Without this line, it was causing a crash on iOS (14?) in release build
 Navigation.events().registerAppLaunchedListener(() => {
     startApp();
 });
