155ca643837be0950f320c24d89bfe130647bc38
Batr Kanzitelli <kanzitdinov@gmail.com>
2020-10-12 16:50:31 +0200

    ExpoApp

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { Navigation } from 'react-native-navigation';
 import { registerRootComponent } from 'expo';
 
 import { startApp } from './src/App';
-import ExpoApp from './src/screens/ExpoAppScreen';
+import ExpoApp from './src/screens/ExpoApp';
 
 registerRootComponent(ExpoApp); // Without this line, it was causing a crash on iOS (14?) in release build
 Navigation.events().registerAppLaunchedListener(() => {
