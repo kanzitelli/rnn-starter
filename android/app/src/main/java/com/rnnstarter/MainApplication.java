22514957dc867e4364a3594daae9dcc88ba28c4b
Batr Kanzitelli <kanzitdinov@gmail.com>
2021-06-22 02:04:04 +0200

    mmkv help class moved to MainApplication
    bc it was causing problem when renaming project

--- android/app/src/main/java/com/rnnstarter/MainApplication.java
+++ android/app/src/main/java/com/rnnstarter/MainApplication.java
@@ -11,9 +11,15 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import java.util.Collections;
 
 import com.facebook.react.bridge.JSIModulePackage;
 import com.swmansion.reanimated.ReanimatedJSIModulePackage;
+
+import com.facebook.react.bridge.JSIModuleSpec;
+import com.facebook.react.bridge.JavaScriptContextHolder;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.reactnativemmkv.MmkvModule;
 
 public class MainApplication extends NavigationApplication {
 
@@ -85,5 +91,16 @@ public class MainApplication extends NavigationApplication {
         e.printStackTrace();
       }
     }
+  }
+}
+
+// TODO: Remove all of this when MMKV and Reanimated can be autoinstalled (maybe RN 0.65)
+// RNNStarterJSIPackage is not in a separate file bc it was causing problem when renaming project
+class RNNStarterJSIPackage extends ReanimatedJSIModulePackage {
+  @Override
+  public List<JSIModuleSpec> getJSIModules(ReactApplicationContext reactApplicationContext, JavaScriptContextHolder jsContext) {
+    super.getJSIModules(reactApplicationContext, jsContext);
+    MmkvModule.install(jsContext, reactApplicationContext.getFilesDir().getAbsolutePath() + "/mmkv");
+    return Collections.emptyList();
   }
 }
