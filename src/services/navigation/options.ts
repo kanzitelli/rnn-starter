332568877383f742ff3dc901603ef6cc0f198224
Batr Kanzitelli <kanzitdinov@gmail.com>
2021-07-01 20:28:18 +0200

    navDefaultOptions added

--- src/services/navigation/options.ts
+++ src/services/navigation/options.ts
@@ -1,8 +1,49 @@
+import { Platform } from 'react-native';
 import { Options, OptionsTopBar } from 'react-native-navigation';
+import { Colors } from 'react-native-ui-lib';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { Button, buttons } from './buttons';
 
 const ICON_SIZE = 25;
+
+export const navDefaultOptions = (): Options => {
+  return {
+    layout: {
+      orientation: ['portrait'],
+      componentBackgroundColor: Colors.bgColor,
+      backgroundColor: Colors.bgColor,
+    },
+    bottomTabs: {
+      titleDisplayMode: 'alwaysShow',
+      backgroundColor: Colors.bgColor,
+    },
+    bottomTab: {
+      iconColor: Colors.primary,
+      textColor: Colors.primary,
+      selectedIconColor: Colors.primary,
+      selectedTextColor: Colors.primary,
+    },
+    topBar: {
+      background: {
+        color: Colors.bgColor,
+      },
+      backButton: {
+        color: Colors.textColor,
+      },
+      // not working
+      // rightButtonColor: Colors.textColor,
+      // leftButtonColor: Colors.textColor,
+      // ===========
+      title: {
+        color: Colors.textColor,
+      },
+      largeTitle: {
+        visible: true,
+        color: Colors.textColor,
+      },
+    },
+  };
+};
 
 export const withBottomTab = (text = 'Screen', icon = 'earth'): Options => ({
   bottomTab: {
