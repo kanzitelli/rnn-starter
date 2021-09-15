5d9341057cf1d5012e5af35a5991b8357dc09544
Batr Kanzitelli <kanzitdinov@gmail.com>
2021-09-15 16:01:29 +0300

    design system dark/light modes only

--- src/utils/designSystem.tsx
+++ src/utils/designSystem.tsx
@@ -1,15 +1,7 @@
-import React, { useEffect } from 'react';
-import { observer } from 'mobx-react';
-import { NavigationComponentProps, NavigationFunctionComponent } from 'react-native-navigation';
+import { Color } from 'react-native-navigation';
 import { Colors, Spacings, Typography } from 'react-native-ui-lib';
-import omit from 'lodash/omit';
 
-import { useServices } from '../services';
-import { stores } from '../stores';
-
-const { ui } = stores;
-
-const colors: ThemeColors = {
+const colors: DesignSystemColors = {
   primary: '#5383b8', // blue
   secondary: '#469c57', // green
   accent: '#fed330', // yellow
@@ -18,52 +10,31 @@ const colors: ThemeColors = {
   whitish: Colors.rgba(250, 250, 250, 1),
   whitish2: Colors.rgba(230, 230, 230, 1),
 };
-const defaultTheme: Theme = {
-  statusBar: 'dark',
-  textColor: colors.blackish,
-  bgColor: colors.whitish,
-  bg2Color: colors.whitish2,
-};
 
-const themeModes: Record<ThemeMode, Theme> = {
-  light: defaultTheme,
+const themes: Record<ThemeMode, ThemeColors> = {
+  light: {
+    textColor: colors.blackish,
+    bgColor: colors.whitish,
+    bg2Color: colors.whitish2,
+  },
   dark: {
-    statusBar: 'light',
     textColor: colors.whitish,
     bgColor: colors.blackish,
     bg2Color: colors.blackish2,
-  },
-  other: {
-    statusBar: 'light',
-    textColor: 'pink',
-    bgColor: 'green',
-    bg2Color: colors.secondary,
   },
 };
 
 // for more information - https://wix.github.io/react-native-ui-lib/foundation/style
 export const configureDesignSystem = (): void => {
-  Colors.loadColors({ ...colors, ...getThemeColors() });
+  Colors.loadColors(colors);
+  Colors.loadSchemes(themes);
 
   Typography.loadTypographies({
     section: { fontSize: 26, fontWeight: '600' },
   });
 };
 
-export const getThemeColors = (): ThemeColors => omit(themeModes[ui.themeMode], 'statusBar');
-export const getTheme = (): Theme => themeModes[ui.themeMode];
-
-export const withThemeModes = (C: NavigationFunctionComponent): NavigationFunctionComponent => {
-  return observer((props: NavigationComponentProps): React.ReactElement => {
-    const { nav } = useServices();
-
-    Colors.loadColors({ ...colors, ...getThemeColors() });
-
-    useEffect(() => {
-      nav.updateDefaultOptions(props.componentId);
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [ui.themeMode]);
-
-    return <C {...props} key={ui.themeMode} />;
-  });
-};
+export const getThemeColor = (c: keyof ThemeColors): Color => ({
+  dark: themes.dark[c],
+  light: themes.light[c],
+});
