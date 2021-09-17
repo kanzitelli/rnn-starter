b570f7e766b4fac826f37172dfb17b120cab5d44
Batr Kanzitelli <kanzitdinov@gmail.com>
2021-09-17 18:59:35 +0200

    translation service better setup logic

--- src/services/translate/index.ts
+++ src/services/translate/index.ts
@@ -2,13 +2,14 @@ import {getLocales} from 'react-native-localize';
 import i18n from 'i18n-js';
 
 import {ru, en} from './translations';
+import {stores} from '../../stores';
 
 export class Translate implements IService {
   private inited = false;
 
   init = async (): PVoid => {
     if (!this.inited) {
-      await this.setup();
+      this.setup();
 
       this.inited = true;
     }
@@ -16,16 +17,20 @@ export class Translate implements IService {
 
   do = i18n.t;
 
-  private setup = async () => {
+  setup = (): void => {
     const locales = getLocales();
 
     if (locales.length > 0) {
+      const {ui} = stores;
       const lng = locales[0].languageCode;
 
       i18n.translations = {en, ru};
-      i18n.locale = lng;
-      // i18n.locale = 'ru';
       i18n.fallbacks = true;
+      if (ui.isSystemLanguage) {
+        i18n.locale = lng;
+      } else {
+        i18n.locale = ui.language;
+      }
     }
   };
 }
