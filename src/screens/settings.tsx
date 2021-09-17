e122474c31dbef137f95f5312103777dac758430
Batr Kanzitelli <kanzitdinov@gmail.com>
2021-09-17 19:00:22 +0200

    UI changed

--- src/screens/settings.tsx
+++ src/screens/settings.tsx
@@ -1,19 +1,40 @@
-import React from 'react';
-import { Alert, Linking, ScrollView } from 'react-native';
-import { View } from 'react-native-ui-lib';
-import { NavigationFunctionComponent } from 'react-native-navigation';
-import { observer } from 'mobx-react';
-import { getApplicationName, getVersion } from 'react-native-device-info';
+import React, {useMemo} from 'react';
+import {Alert, Linking, ScrollView} from 'react-native';
+import {View, ActionSheet, Text} from 'react-native-ui-lib';
+import {NavigationFunctionComponent} from 'react-native-navigation';
+import {observer, useLocalObservable} from 'mobx-react';
+import {getApplicationName, getVersion} from 'react-native-device-info';
 
-import { useConstants } from '../utils/constants';
-import { useStores } from '../stores';
+import {useConstants} from '../utils/constants';
+import {useStores} from '../stores';
 
-import { Section } from '../components/section';
-import { Action } from '../components/action';
+import {Section} from '../components/section';
+import {Action} from '../components/action';
+
+type PickersStateKey = keyof Omit<PickersState, 'show' | 'hide'>;
+type PickersState = {
+  appearance: boolean;
+  language: boolean;
+
+  show: <T extends PickersStateKey>(what: T) => void;
+  hide: <T extends PickersStateKey>(what: T) => void;
+};
 
 export const Settings: NavigationFunctionComponent = observer(() => {
-  const { ui } = useStores();
-  const { links } = useConstants();
+  const {ui} = useStores();
+  const {links} = useConstants();
+
+  const pickers: PickersState = useLocalObservable(() => ({
+    appearance: false,
+    language: false,
+
+    show<T extends PickersStateKey>(what: T) {
+      pickers[what] = true;
+    },
+    hide<T extends PickersStateKey>(what: T) {
+      pickers[what] = false;
+    },
+  }));
 
   const doSomething = (action: string) => () => {
     Alert.alert(action);
@@ -23,6 +44,79 @@ export const Settings: NavigationFunctionComponent = observer(() => {
     Linking.openURL(link);
   };
 
+  const appearancePickOption = (option: UIAppearance) => () => {
+    ui.setAppearanceMode(option);
+    console.log(option);
+  };
+
+  const languagePickOption = (option: UILanguage) => () => {
+    ui.setLanguage(option);
+    console.log(option);
+  };
+
+  const appearanceActions: AppearanceAction[] = useMemo(
+    () => [{name: 'System'}, {name: 'Light'}, {name: 'Dark'}],
+    [],
+  );
+  const AppearanceActionSheet = useMemo(
+    () => (
+      <ActionSheet
+        title={'Appearance'}
+        cancelButtonIndex={appearanceActions.length}
+        useNativeIOS
+        options={[
+          ...appearanceActions.map(action => ({
+            label: action.name,
+            onPress: appearancePickOption(action.name),
+          })),
+          {
+            label: 'Cancel',
+          },
+        ]}
+        visible={pickers.appearance}
+        onDismiss={() => pickers.hide('appearance')}
+      />
+    ),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [pickers.appearance],
+  );
+
+  const languageActions: LanguageAction[] = useMemo(
+    () => [{name: 'System'}, {name: 'English'}, {name: 'Russian'}],
+    [],
+  );
+  const LanguageActionSheet = useMemo(
+    () => (
+      <ActionSheet
+        title={'Language'}
+        cancelButtonIndex={languageActions.length}
+        useNativeIOS
+        options={[
+          ...languageActions.map(action => ({
+            label: action.name,
+            onPress: languagePickOption(action.name),
+          })),
+          {
+            label: 'Cancel',
+          },
+        ]}
+        visible={pickers.language}
+        onDismiss={() => pickers.hide('language')}
+      />
+    ),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [pickers.language],
+  );
+
+  const UINote = useMemo(
+    () => (
+      <View paddingH-s3 marginB-s4>
+        <Text grey40>Changing UI options will reload the app</Text>
+      </View>
+    ),
+    [],
+  );
+
   return (
     <View flex bg-bgColor>
       <ScrollView contentInsetAdjustmentBehavior="always">
@@ -30,12 +124,21 @@ export const Settings: NavigationFunctionComponent = observer(() => {
           <Section bg title="UI">
             <Action
               title="Appearance"
-              info={ui.themeMode}
-              onPress={ui.toggleThemeMode}
+              info={ui.appearanceName}
+              onPress={() => pickers.show('appearance')}
               rightIcon="chevron-forward"
             />
-            {/* <Action title="Language" info={'en'} onPress={() => {}} /> */}
+            {AppearanceActionSheet}
+
+            <Action
+              title="Language"
+              info={ui.languageName}
+              onPress={() => pickers.show('language')}
+              rightIcon="chevron-forward"
+            />
+            {LanguageActionSheet}
           </Section>
+          {UINote}
 
           <Section bg title="General">
             <View>
