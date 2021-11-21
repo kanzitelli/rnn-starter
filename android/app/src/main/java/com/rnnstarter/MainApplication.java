fda79a59f7f876a9e3f65c3ba4445d4c2bd768c8
Batr Kanzitelli <kanzitdinov@gmail.com>
2021-11-21 20:35:48 +0100

    [Android] expo modules setup

--- android/app/src/main/java/com/rnnstarter/MainApplication.java
+++ android/app/src/main/java/com/rnnstarter/MainApplication.java
@@ -21,6 +21,9 @@ import com.facebook.react.bridge.JavaScriptContextHolder;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.reactnativemmkv.MmkvModule;
 
+import expo.modules.ApplicationLifecycleDispatcher;
+import android.content.res.Configuration;
+
 public class MainApplication extends NavigationApplication {
 
   private final ReactNativeHost mReactNativeHost =
@@ -60,7 +63,14 @@ public class MainApplication extends NavigationApplication {
     super.onCreate();
     
     // initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+    ApplicationLifecycleDispatcher.onApplicationCreate(this);
   }
+
+  @Override
+	public void onConfigurationChanged(Configuration newConfig) {
+	  super.onConfigurationChanged(newConfig);
+	  ApplicationLifecycleDispatcher.onConfigurationChanged(this, newConfig);
+	}
 
   /**
    * Loads Flipper in React Native templates. Call this in the onCreate method with something like
