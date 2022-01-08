315a232d2c3c525fbd25c17a2432da164c7997f7
Batr Kanzitelli <kanzitdinov@gmail.com>
2022-01-08 01:11:48 +0100

    comment removed

--- ios/RNNStarter/AppDelegate.h
+++ ios/RNNStarter/AppDelegate.h
@@ -2,7 +2,6 @@
 #import <Expo/Expo.h>
 #import <UIKit/UIKit.h>
 
-//@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
 @interface AppDelegate : EXAppDelegateWrapper <UIApplicationDelegate, RCTBridgeDelegate>
 
 @property (nonatomic, strong) UIWindow *window;
