fc9dcb98093a2ea0ae270b2a40f6e18745a1994c
Batyr Kanzitdinov <dev@batyr.io>
2022-08-29 22:35:58 +0200

    `stores` added

--- src/stores/_hydration.ts
+++ src/stores/_hydration.ts
@@ -1,5 +1,4 @@
 import {MMKV} from 'react-native-mmkv';
-// import AsyncStorage from '@react-native-async-storage/async-storage';
 import {configurePersistable} from 'mobx-persist-store';
 
 // for safe AsyncStorage --> MMKV migration, checkout this https://github.com/mrousavy/react-native-mmkv/issues/52
@@ -15,22 +14,3 @@ configurePersistable({
     removeItem: key => storage.delete(key),
   },
 });
-
-// AsyncStorage configuration
-// configurePersistable({
-//   debugMode: __DEV__,
-//   storage: {
-//     setItem: async (key, value) => {
-//       await AsyncStorage.setItem(key, value);
-//       return Promise.resolve();
-//     },
-//     getItem: async (key) => {
-//       const value = await AsyncStorage.getItem(key);
-//       return Promise.resolve(value);
-//     },
-//     removeItem: async (key) => {
-//       await AsyncStorage.removeItem(key);
-//       return Promise.resolve();
-//     },
-//   },
-// });
