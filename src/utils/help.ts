2d8876fc882bb84dbe07e6b16b0be5db0c5f5ae2
Batyr Kanzitdinov <dev@batyr.io>
2022-10-19 20:57:28 +0200

    small changes

--- src/utils/help.ts
+++ src/utils/help.ts
@@ -1,10 +1,5 @@
 import RNRestart from 'react-native-restart';
-import SplashScreen from 'react-native-splash-screen'
 
 export const randomNum = (max = 100): number => Math.floor(Math.random() * max);
 
-export const restartApp = () => {
-    //show splash screen before restarting
-    SplashScreen.show();
-    RNRestart.Restart();
-};
+export const restartApp = () => RNRestart.Restart();
