60522e9f997158479f7de715645afa4c0c96eb7d
Batyr Kanzitdinov <dev@batyr.io>
2022-10-20 14:25:01 +0200

    titles that use `translate` service moved

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -4,15 +4,11 @@ import {gestureHandlerRootHOC as withGestureHandler} from 'react-native-gesture-
 import {Main} from './main';
 import {Settings} from './settings';
 
-import {withServices} from '../services';
-import {withStores} from '../stores';
-import {
-  withBottomTab,
-  withRightButtons,
-  withTitle,
-} from '../services/navigation/options';
+import {withBottomTab, withRightButtons} from '../services/navigation/options';
 import {Sample} from './_screen-sample';
 import {Playground} from './playground';
+import {withAppearance} from '../utils/hooks';
+import {withSS} from '../utils/providers';
 
 export const screens = generateRNNScreens(
   {
@@ -20,7 +16,10 @@ export const screens = generateRNNScreens(
       component: Main,
       options: {
         topBar: {
-          ...withTitle('Main'),
+          // title is set in services/navigation/index.ts
+          background: {
+            color: 'red',
+          },
           ...withRightButtons('inc', 'dec'),
         },
         ...withBottomTab('Main', 'home'),
@@ -29,18 +28,14 @@ export const screens = generateRNNScreens(
     Playground: {
       component: Playground,
       options: {
-        topBar: {
-          ...withTitle('Playground'),
-        },
+        topBar: {title: {text: 'Playground'}},
         ...withBottomTab('Playground', 'construct'),
       },
     },
     Settings: {
       component: Settings,
       options: {
-        topBar: {
-          ...withTitle('Settings'),
-        },
+        topBar: {title: {text: 'Settings'}},
         ...withBottomTab('Settings', 'settings'),
       },
     },
@@ -49,10 +44,13 @@ export const screens = generateRNNScreens(
       component: Sample,
       options: {
         topBar: {
-          ...withTitle('Sample'),
+          title: {text: 'Sample'},
+          background: {
+            color: 'yellow',
+          },
         },
       },
     },
   },
-  [withGestureHandler, withStores, withServices],
+  [withGestureHandler, withSS, withAppearance],
 );
